bd3786ab5dfc5e03c8b4cf179dfbc47107ba6b47
asterzwx <1702659@sit.singaporetech.edu.sg>
2019-01-14 15:54:22 +0800

    added log.d code

--- app/src/main/java/com/example/aster/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aster/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aster.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: ");
     }
+
+    private static final String TAG = "MainActivity";
+
 }
